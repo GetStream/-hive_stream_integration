7bee20baf1d47bf3c2f0f962dd577feca733fb91
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-06-03 14:02:44 -0400

    webhook server wip

--- main.py
+++ main.py
@@ -1,55 +1,31 @@
-# Imports:
+import uvicorn
 import os
-import requests
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from stream_chat import StreamChat
 
-# Used to call most Hive APIs
-from hive_response import HiveResponse, hive_response_from_dict
-from moderation import ModerationElement, moderation_to_dict
-
-# Inputs:
-# The unique API Key for your text project
+from event import event_from_dict
+from hive import get_hive_response
 API_Key = os.environ.get('HIVE_API_KEY')
 
-
-def get_hive_response(input_text) -> ModerationElement:
-    headers = {'Authorization': f'Token {API_Key}'}
-    # Must be a string. This is also where you would insert metadata if desired.
-    data = {'text_data': input_text}
-    # Submit request to the synchronous API endpoint.
-    response = requests.post(
-        'https://api.thehive.ai/api/v2/task/sync', headers=headers, data=data)
-    response_dict = response.json()
-  
-    result = hive_response_from_dict(response_dict)
-    input = ""
-    moderation_class = None
-    score = None
-    is_moderated = False
-    moderated_word = None
-    reason = None
-    for status in result.status:
-        input = status.response.input.text
-
-        for output in status.response.output:
-
-            for class_ in output.classes:
-
-                if class_.score > 0:
-                    moderation_class = class_.class_class
-                    score = class_.score
-        filters = status.response.text_filters
-        is_moderated = len(filters) > 0
-        for text_filter in status.response.text_filters:
-            moderated_word = text_filter.value
-            reason = text_filter.type
-
-    moderation = ModerationElement(
-        input, moderation_class, score, is_moderated, moderated_word, reason)
-    return moderation
+your_api_key = os.environ.get('api_key')
+your_api_secret = os.environ.get('api_secret')
 
 
-# moderation = get_hive_response("you are a nice guy")
-# if(moderation.is_moderated):
-    # stream_chat_client.flag_message(msg["id"], user_id=server_user["id"])
+chat = StreamChat(api_key=your_api_key, api_secret=your_api_secret)
+app = Starlette(debug=True)
 
 
+@app.route('/chatEvent', methods=['POST'])
+async def chatEvent(request):
+    data = await request.json()
+    event = event_from_dict(data)
+    if event.type == "message.new":
+        text = event.message.text
+        moderation = get_hive_response(text)
+        if(moderation.is_moderated):
+            print("flagged")
+    return JSONResponse({"received": data})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
