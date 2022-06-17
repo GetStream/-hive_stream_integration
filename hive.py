38a5428c9168282177c608590fe981c70c4497b9
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-06-17 16:19:14 -0400

    get rid of response helpers

--- hive.py
+++ hive.py
@@ -3,14 +3,12 @@
 import requests
 
 # Used to call most Hive APIs
-from hive_response import hive_response_from_dict
-from moderation import ModerationElement
 
 # Inputs:
 # The unique API Key for your text project
 
 
-def get_hive_response(input_text,API_Key) -> ModerationElement:
+def get_hive_response(input_text,API_Key):
     headers = {'Authorization': f'Token {API_Key}'}
     # Must be a string. This is also where you would insert metadata if desired.
     data = {'text_data': input_text}
@@ -18,36 +16,4 @@ def get_hive_response(input_text,API_Key) -> ModerationElement:
     response = requests.post(
         'https://api.thehive.ai/api/v2/task/sync', headers=headers, data=data)
     response_dict = response.json()
-  
-    result = hive_response_from_dict(response_dict)
-    input = ""
-    moderation_class = None
-    score = None
-    is_moderated = False
-    moderated_word = None
-    reason = None
-    for status in result.status:
-        input = status.response.input.text
-
-        for output in status.response.output:
-
-            for class_ in output.classes:
-
-                if class_.score > 0:
-                    moderation_class = class_.class_class
-                    score = class_.score
-        filters = status.response.text_filters
-        is_moderated = len(filters) > 0
-        for text_filter in status.response.text_filters:
-            moderated_word = text_filter.value
-            reason = text_filter.type
-
-    moderation = ModerationElement(
-        input, moderation_class, score, is_moderated, moderated_word, reason)
-    return moderation
-
-
-# moderation = get_hive_response("you are a nice guy")
-# if(moderation.is_moderated):
-    # stream_chat_client.flag_message(msg["id"], user_id=server_user["id"])
-
+    return response_dict
--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from starlette.responses import JSONResponse
 from stream_chat import StreamChat
 from decimal import *
 from hive import get_hive_response
-threshold = Decimal(0.9)
+visual_threshold = Decimal(0.9)
+text_ban_threshold = 3
+text_flag_threshold = 1
 
 # from hive import get_hive_response
 from visual_moderation import get_visual_hive_response
@@ -27,19 +29,19 @@ async def chatEvent(request):
         text = data["message"]["text"]
         attachments = data["message"]["attachments"]
         user = data["message"]["user"]
-        print(user)
         banned = False
+        flagged = False
         reason=""
         for attachment in attachments:
             print(attachment["type"])
             if attachment["type"] == "image":
-                dic = get_visual_hive_response(attachment["image_url"],hive_visual_api_key)
-                for output in dic["status"][0]["response"]["output"]:
+                visual_hive_response = get_visual_hive_response(attachment["image_url"],hive_visual_api_key)
+                for output in visual_hive_response["status"][0]["response"]["output"]:
                     for class_ in output["classes"]:
                         score = class_["score"]
                         decimal_score = Decimal(score)
                         
-                        if Decimal(threshold).compare(decimal_score) == -1:
+                        if Decimal(visual_threshold).compare(decimal_score) == -1:
                             print(class_["class"])
                             print(class_["score"])
                             if class_["class"] == "general_nsfw":
@@ -48,14 +50,33 @@ async def chatEvent(request):
                             if class_["class"] == "general_suggestive":
                                 reason = "suggestive"
                                 banned = True
+
+        hive_response = get_hive_response(
+            text, hive_api_key)
+        filters = hive_response["status"][0]["response"]["text_filters"]
+        for text_filter in filters:
+            moderated_word = text_filter["value"]
+            print("moderated word: "+moderated_word)
+            print("filtered : "+text_filter["type"])
+            flagged = len(filters) > 0
+        for output in hive_response["status"][0]["response"]["output"]:            
+                    for class_ in output["classes"]:
+                        print(class_["class"])
+                        print(class_["score"])
+                        score = class_["score"]
+                        if score >= text_flag_threshold:
+                            print("FLAG")
+                            flagged = True
+                            if score >= text_ban_threshold:
+                                banned = True
+                                reason = class_["class"]
+                                print("BAN")
+        if flagged:
+            chat.flag_message(data["message"]["id"], user_id="hive-bot")
         if banned:
             print(banned)
             chat.ban_user(user["id"], banned_by_id="hive-bot", timeout=24*60, reason=reason)
-        text_moderation = get_hive_response(
-            text, hive_api_key)
-        if(text_moderation.is_moderated):
-            print("flagged")
-            chat.flag_message(data["message"]["id"], user_id="hive-bot")
+               
     return JSONResponse({"received": data})
 
 if __name__ == "__main__":
